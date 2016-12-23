a5c568fc3135446bfe2afa341545d3b33cb100a7
Mikael Nystrom <deploymentbunny@outlook.com>
2016-12-23 13:14:29 +0100

    Update

--- VIAStorageModule.psm1
+++ VIAStorageModule.psm1
@@ -13,17 +13,18 @@ Function Initialize-VIADataDisk
     Param(
         [parameter(position=0,mandatory=$true)]
         [ValidateNotNullOrEmpty()]
-        [ValidateSet('NTFS','ReFS')]
+        [ValidateSet('NTFS','ReFS','NA')]
         $FileSystem = 'NTFS',
         $DiskNumber = '1',
         $PartitionType = 'GPT',
         $FileSystemLabel = 'Datadisk01'
     )
-
     Write-Verbose "Working on disk: $DiskNumber"
-    Initialize-Disk -Number $DiskNumber –PartitionStyle $PartitionType
-    $Drive = New-Partition -DiskNumber $DiskNumber -GptType '{ebd0a0a2-b9e5-4433-87c0-68b6b72699c7}' -UseMaximumSize
-    $Drive | Format-Volume -FileSystem $FileSystem -NewFileSystemLabel $FileSystemLabel
-    Add-PartitionAccessPath -DiskNumber $DiskNumber -PartitionNumber $Drive.PartitionNumber -AssignDriveLetter
-    $Drive = Get-Partition -DiskNumber $DiskNumber -PartitionNumber $Drive.PartitionNumber
+    if($FileSystem -eq 'NA'){}else{
+        Initialize-Disk -Number $DiskNumber –PartitionStyle $PartitionType
+        $Drive = New-Partition -DiskNumber $DiskNumber -GptType '{ebd0a0a2-b9e5-4433-87c0-68b6b72699c7}' -UseMaximumSize
+        $Drive | Format-Volume -FileSystem $FileSystem -NewFileSystemLabel $FileSystemLabel
+        Add-PartitionAccessPath -DiskNumber $DiskNumber -PartitionNumber $Drive.PartitionNumber -AssignDriveLetter
+        $Drive = Get-Partition -DiskNumber $DiskNumber -PartitionNumber $Drive.PartitionNumber
+    }
 }
